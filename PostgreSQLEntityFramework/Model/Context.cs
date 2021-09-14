45e7caad6a8a5380dffdb0b6767c4012c15acc72
Emre <eemmrree0@outlook.com>
2021-09-14 19:41:08 +0300

    PostgreSQLEntityFramework projesinde Model klasöründe Context sınıfı oluşturuldu.

---
+++ PostgreSQLEntityFramework/Model/Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostgreSQLEntityFramework.Model
+{
+    public class Context: DbContext
+    {
+        public Context() : base("name=Context")
+        {
+
+        }
+
+        public DbSet<Birim> Birims { get; set; }
+        public DbSet<Personel> Personels { get; set; }
+    }
+}
