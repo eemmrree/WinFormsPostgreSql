fe17ac839c162ebe2fd67a640ee1dee36b5fc132
Emre <eemmrree0@outlook.com>
2021-09-14 17:53:14 +0300

    Kategori formunda kategorileri listeleme ve ekleme işlemi yapıldı.

--- WinFormPostgreSql/FrmKategori.cs
+++ WinFormPostgreSql/FrmKategori.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
 
 namespace WinFormPostgreSql
 {
@@ -15,6 +16,28 @@ namespace WinFormPostgreSql
         public FrmKategori()
         {
             InitializeComponent();
+        }
+        NpgsqlConnection npgsqlConnection = new NpgsqlConnection("server=localhost; port=5432; Database=dburun; user ID=postgres; password=yok1;");
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            npgsqlConnection.Open();
+            NpgsqlCommand command1 =
+                new NpgsqlCommand("insert into kategori (kategoriid, kategoriad) values (@p1 ,@p2)", npgsqlConnection);
+
+            command1.Parameters.AddWithValue("@p1",int.Parse(txtKategoriID.Text));
+            command1.Parameters.AddWithValue("@p2", txtKategoriAd.Text);
+            command1.ExecuteNonQuery();
+            npgsqlConnection.Close();
+            MessageBox.Show("Ekleme işlemi Başarılı");
+        }
+
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            string sorgu = "select * from kategori";
+            NpgsqlDataAdapter dataAdapter = new NpgsqlDataAdapter(sorgu, npgsqlConnection);
+            DataSet dataSet = new DataSet();
+            dataAdapter.Fill(dataSet);
+            dataGridView1.DataSource = dataSet.Tables[0];
         }
     }
 }
