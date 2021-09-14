1d9f10952e2bb01d0734a2f16185030e1b238560
Emre <eemmrree0@outlook.com>
2021-09-14 19:39:48 +0300

    PostgreSQLEntityFramework projesinde Model klasöründe Personel propertyleri tanımlandı.

---
+++ PostgreSQLEntityFramework/Model/Personel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostgreSQLEntityFramework.Model
+{
+    public class Personel
+    {
+        [Key]
+        public int personelid { get; set; }
+        public string personelad { get; set; }
+        public string personelsoyad { get; set; }
+        public string birim { get; set; }
+    }
+}
