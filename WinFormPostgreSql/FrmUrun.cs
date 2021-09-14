2c11100bddb71a298e96b4299ba141bbc0329e7f
Emre <eemmrree0@outlook.com>
2021-09-14 17:54:07 +0300

    Ürün formunda Listeleme işlemi yapıldı.

--- WinFormPostgreSql/FrmUrun.cs
+++ WinFormPostgreSql/FrmUrun.cs
@@ -7,14 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
+
 
 namespace WinFormPostgreSql
 {
     public partial class FrmUrun : Form
     {
+     
         public FrmUrun()
         {
             InitializeComponent();
+            
+        }
+
+        NpgsqlConnection npgsqlConnection = new NpgsqlConnection("server=localhost; port=5432; Database=dburun; user ID=postgres; password=yok1;");
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            string sorgu = "select * from urunler";
+          NpgsqlDataAdapter dataAdapter = new NpgsqlDataAdapter(sorgu , npgsqlConnection);
+          DataSet dataSet = new DataSet();
+          dataAdapter.Fill(dataSet);
+          dataGridView1.DataSource = dataSet.Tables[0];
+
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
